c435bdd22369be07ba89dd74b426b7a2ebf19325
Sergey Beryozkin <sberyozkin@gmail.com>
2022-02-22 18:54:08 +0000

    Add integration tests

---
+++ integration-tests/src/test/java/io/quarkiverse/filevault/it/FileVaultIT.java
@@ -0,0 +1,10 @@
+package io.quarkiverse.filevault.it;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class FileVaultIT extends FileVaultTestCase {
+
+    // Runs the same tests as the parent class
+
+}
